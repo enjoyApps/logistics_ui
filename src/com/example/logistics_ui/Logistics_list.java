0af7c442403a3e1824637ec1cbebd35b64f8f38a
noahlu <noahseo@gmail.com>
2012-12-15 12:47:28 +0800

    update

--- src/com/example/logistics_ui/Logistics_list.java
+++ src/com/example/logistics_ui/Logistics_list.java
@@ -43,27 +43,27 @@ public class Logistics_list extends ListActivity {
         setListAdapter(new ArrayAdapter<String>(this,
         	android.R.layout.simple_list_item_1, logistics_list));
         
-        s1 = (Spinner) findViewById(R.id.spinner1);
-        
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-            android.R.layout.simple_spinner_item, logistics_list);
- 
-        s1.setAdapter(adapter);
-        OnItemSelectedListener onItemSelectedListener = new OnItemSelectedListener()
-        {
-            public void onItemSelected(AdapterView<?> arg0, 
-            View arg1, int arg2, long arg3) 
-            {
-                int index = s1.getSelectedItemPosition();
-                Toast.makeText(getBaseContext(), 
-                    "You have selected item : " + logistics_list[index], 
-                    Toast.LENGTH_SHORT).show();                
-            }
- 
-            public void onNothingSelected(AdapterView<?> arg0) {}
-
-        };
-        s1.setOnItemSelectedListener(onItemSelectedListener);
+//        s1 = (Spinner) findViewById(R.id.spinner1);
+//        
+//        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+//        		android.R.layout.simple_spinner_dropdown_item, logistics_list);
+// 
+//        s1.setAdapter(adapter);
+//        OnItemSelectedListener onItemSelectedListener = new OnItemSelectedListener()
+//        {
+//            public void onItemSelected(AdapterView<?> arg0, 
+//            View arg1, int arg2, long arg3) 
+//            {
+//                int index = s1.getSelectedItemPosition();
+//                Toast.makeText(getBaseContext(), 
+//                    "You have selected item : " + logistics_list[index], 
+//                    Toast.LENGTH_SHORT).show();                
+//            }
+// 
+//            public void onNothingSelected(AdapterView<?> arg0) {}
+//
+//        };
+//        s1.setOnItemSelectedListener(onItemSelectedListener);
         
     }    
  
