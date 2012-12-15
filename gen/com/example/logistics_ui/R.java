43bffb96ea038b7e73128775b8727dfa82f8c0ac
noahlu <noahseo@gmail.com>
2012-12-15 13:48:21 +0800

    add

--- gen/com/example/logistics_ui/R.java
+++ gen/com/example/logistics_ui/R.java
@@ -84,6 +84,7 @@ public final class R {
         public static final int activity_logistics_manager=0x7f030004;
         public static final int activity_parcel_list=0x7f030005;
         public static final int activity_send_parcel=0x7f030006;
+        public static final int list_row=0x7f030007;
     }
     public static final class menu {
         public static final int activity_account_login=0x7f070000;
